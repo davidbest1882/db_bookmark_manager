6466db25c091f021b4435fd82f2d35a1122e8126
David Best <david_best@ntlworld.com>
2018-10-24 13:07:38 +0100

    completed exercise 11 walkthrough but with errors

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -7,15 +7,15 @@ feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
 
     # Added the test data
-    Bookmark.create(url: "http://www.makersacademy.com")
-    Bookmark.create(url: "http://www.destroyallsoftware.com")
-    Bookmark.create(url: "http://www.google.com")
+    Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+    Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+    Bookmark.create(url: "http://www.google.com", title: "Google")
 
 
     visit('/bookmarks')
 
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.google.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_link('Makers Academy', href: "http://www.makersacademy.com")
+    expect(page).to have_link('Google', href: "http://www.google.com")
+    expect(page).to have_link('Destory All Software', href: "http://www.destroyallsoftware.com")
   end
 end
