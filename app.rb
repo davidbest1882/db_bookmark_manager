2ec705fb9b8f40b39deb81b3231aa07771ea3236
David Best <david_best@ntlworld.com>
2018-10-25 14:44:38 +0100

    Completed exercise 12

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require 'sinatra/base'
 require './lib/bookmark'
 
+
+
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions, :method_override
+
   get '/' do
     'Bookmark Manager'
   end
@@ -18,6 +23,11 @@ class BookmarkManager < Sinatra::Base
 
   get '/bookmarks/new' do
     erb :'bookmarks/new'
+  end
+
+  delete '/bookmarks/:id' do
+    Bookmark.delete(id: params[:id])
+    redirect '/bookmarks'
   end
 
   run if app_file == $0
